65cd1a08204a4a2b95797896670c1f662f3848fa
Jennifer Naasz <naasz.jl@gmail.com>
2025-01-21 11:38:06 -0500

    feat(Protein Tracker): Begin adding landing page

--- app/src/main/java/com/example/gains/MainActivity.kt
+++ app/src/main/java/com/example/gains/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.gains
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gains.ui.theme.GainsTheme
+import com.example.gains.ui.theme.medium
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +24,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             GainsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background // Color(0xFF6650a4)
+                )
+                {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        ProteinGoal(88, 130)
+                        Steps(84444, 10000)
+                    }
                 }
             }
         }
@@ -27,14 +44,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ProteinGoal(current: Int, goal: Int) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .requiredSize(100.dp) // Enforces the size to not stretch
+            .background(color = medium, shape = CircleShape)
+    ) {
+        Text(
+            text = "$current / $goal",
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun Steps(current: Int, goal: Int) {
+    val remaining: Int = current - goal
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Steps: $current")
+        Text(text = "$remaining remaining of $goal")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GainsTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        )
+        {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                ProteinGoal(88, 130)
+                Steps(84444, 10000)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/gains/ui/theme/Color.kt
+++ app/src/main/java/com/example/gains/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.gains.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// dark color theme
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+// light color theme
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Background40 = Color(0xFFF5DDDD)
+
+// progress colors
+val low = Color(0xB00B10)
+val medium = Color(0xFFE6E20E)
+val high = Color(0xFF117D2E)
