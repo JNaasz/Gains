7bf3625fc7a7144dd68dc5bf676feca207582d8a
Jennifer Naasz <naasz.jl@gmail.com>
2025-02-28 13:50:44 -0500

    chore: remove unused imports

--- app/src/main/kotlin/com/example/gains/features/nutrition/LogNutritionViewModel.kt
+++ app/src/main/kotlin/com/example/gains/features/nutrition/LogNutritionViewModel.kt
@@ -10,11 +10,9 @@ import com.example.gains.database.ProteinSource
 import com.example.gains.features.nutrition.Util.calculateProtein
 import com.example.gains.features.nutrition.Util.epochMilliToLocalDate
 import com.example.gains.features.nutrition.Util.getMatchedSelectionData
-import com.example.gains.features.nutrition.Util.mergeSourceList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import javax.inject.Inject
